a7f3c7de7813b513bb68adb6b3ee428d0bc8d253
prajju2008 <prajju2008@gmail.com>
2021-08-16 00:30:53 +0530

    add dependancy

--- src/Parameter.groovy
+++ src/Parameter.groovy
@@ -1,4 +1,4 @@
-@Grab(group = 'software.amazon.awscdk.services', module = 'ssm', version = '2.16.2')
+@Grab(group='software.amazon.awssdk', module='ssm', version='2.17.19')
 
 import software.amazon.awscdk.services.ssm.StringParameter
 
