7a7b90a78b45f96be9680e3ef5b921dfb86d8f16
prajju2008 <prajju2008@gmail.com>
2021-08-16 03:09:50 +0530

    add config file

--- src/StringParameter.groovy
+++ src/StringParameter.groovy
@@ -1,5 +1,5 @@
-@Grab(group='software.amazon.awssdk', module='ssm', version='2.17.19')
 @Grab(group='software.amazon.awssdk', module='services', version='2.17.19')
+@Grab(group='software.amazon.awssdk', module='ssm', version='2.17.19')
 
 import software.amazon.awscdk.services.ssm.*
 
