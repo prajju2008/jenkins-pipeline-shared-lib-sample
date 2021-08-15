edf4a62c26b5ae8de891e8251e8c6c04f2e57037
prajju2008 <prajju2008@gmail.com>
2021-08-16 02:47:00 +0530

    test

--- src/StringParameter.groovy
+++ src/StringParameter.groovy
@@ -1,6 +1,6 @@
-@Grapes(
-  @Grab(group='software.amazon.awssdk', module='ssm', version='2.17.19')
-)
+@Grab(group='software.amazon.awssdk', module='ssm', version='2.17.19')
+@Grab(group='software.amazon.awssdk', module='services', version='2.17.19')
+
 import software.amazon.awscdk.services.ssm.*
 
 class StringParameter {
