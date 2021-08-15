3c14359a2b97fe70c4f75fb573d089a5da793454
prajju2008 <prajju2008@gmail.com>
2021-08-16 01:04:29 +0530

    lets try

---
+++ src/StringParameter.groovy
@@ -0,0 +1,15 @@
+@Grab(group='software.amazon.awssdk', module='ssm', version='2.17.19')
+
+import software.amazon.awscdk.ssm.StringParameter
+
+class StringParameter {
+    Script script
+
+    def run() {
+
+        String secureStringToken = StringParameter.valueForSecureStringParameter(this, "test", 1); 
+        
+        script.echo("securestring is " + secureStringToken)
+        
+    }
+}
